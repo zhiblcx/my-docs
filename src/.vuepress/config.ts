1a84c2f7599eee37d9e6be32b6e94981332f388c
zhiblcx <1529088692@qq.com>
2023-08-13 22:37:23 +0800

    feat:some component

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,14 @@
+import { defineUserConfig } from 'vuepress'
+import theme from './theme.js'
+
+export default defineUserConfig({
+  base: '/',
+
+  lang: 'zh-CN',
+  title: '博客',
+  description: 'vuepress-theme-hope 的博客演示',
+  theme
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+})
